231982a2483677ef99c611b5d5c3a80bc37e01a6
Sumit Arora <er.sumitarora@gmail.com>
2019-08-28 14:18:55 -0400

    adding custom pipe example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  fileSizes = [10, 100, 1000, 10000, 100000, 10000000, 10000000000];
+  largeFileSize = Math.pow(10, 15)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FormatFileSizePipe } from './file-size.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, FormatFileSizePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
