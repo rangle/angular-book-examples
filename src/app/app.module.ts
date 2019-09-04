dad922650a10639f626fd87b8739c74dd41c6d97
Sumit Arora <er.sumitarora@gmail.com>
2019-09-04 14:38:26 -0400

    Adding Child Routes Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { routing } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import ComponentOne from './component-one';
+import ComponentTwo from './component-two';
+import ChildOne from './child-one';
+import ChildTwo from './child-two';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, routing],
+  declarations: [AppComponent, HelloComponent, ComponentOne, ComponentTwo, ChildOne, ChildTwo],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/child-two.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'child-two',
+  template: 'Child Two',
+})
+export default class ChildTwo {}
