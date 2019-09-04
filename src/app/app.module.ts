7a6e4f0c68e85cb403dcfce300f91bb3f7da6323
Sumit Arora <er.sumitarora@gmail.com>
2019-09-04 09:56:35 -0400

    Adding basic router example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { routing } from './app.routes';
+import ComponentOne from './component-one';
+import ComponentTwo from './component-two';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, routing],
+  declarations: [AppComponent, HelloComponent, ComponentOne, ComponentTwo],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
